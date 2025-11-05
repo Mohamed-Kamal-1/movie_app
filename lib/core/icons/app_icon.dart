736b51907e6d0a2106c79392ed27d5e41129cafe
moham <mk22443388@gmail.com>
2025-11-05 13:17:02 +0200

    feat: Update bottom navigation icons and constants
    This commit refactors the icon assets for the bottom navigation bar by replacing the old home icon and adding several new ones. It also updates the `AppIcon` constants to reflect these changes.
    
    - **Asset Changes:**
      - Deleted the old home icon (`Home 1.svg`).
      - Added new SVG icons for `home_tab`, `sreach_tab` (search), `compass`, and `profile`.
    
    - **Code Changes:**
      - Updated `AppIcon.dart` to define new constants (`ic_home`, `ic_search`, `ic_compass`, `ic_profile`) pointing to the new asset paths.

--- lib/core/icons/app_icon.dart
+++ lib/core/icons/app_icon.dart
@@ -8,5 +8,11 @@ class AppIcon{
 
   static const String ic_google  = '${_basePath}google.svg';
 
+  static const String _basePathBottomNavigation = 'assets/images/images_app/icon_app/bottom_navigation/';
+  static const String ic_home  = '${_basePathBottomNavigation}home_tab.svg';
+  static const String ic_search = '${_basePathBottomNavigation}sreach_tab.svg';
+  static const String ic_compass  = '${_basePathBottomNavigation}compass.svg';
+  static const String ic_profile  = '${_basePathBottomNavigation}profile.svg';
+
 
 }
