e1d1a5c1d5977fd3f87b73692030e90d8225aded
moham <mk22443388@gmail.com>
2025-12-04 18:02:27 +0200

    refactor: Delete AvailableNowSection and style search bar
    This commit removes the unused `AvailableNowSection` widget and updates the styling of the search bar in the `SearchScreen`.
    
    Key changes:
    - **`available_now_section.dart`**: Deleted the entire file, which contained a stateful widget with an animated `PageView` for displaying images.
    - **`app_bar_sear_screen.dart`**:
        - Wrapped the `AppBar` with a `Theme` widget to ensure consistent styling.
        - Set the `cursorColor` of the `TextField` to `AppColor.yellow` for better visibility.

--- lib/presentation/ui/home_screen/tabs/search_tab/app_bar_sear_screen.dart
+++ lib/presentation/ui/home_screen/tabs/search_tab/app_bar_sear_screen.dart
@@ -10,38 +10,43 @@ class AppBarSearchWidget extends StatelessWidget
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      flexibleSpace: Padding(
-        padding: const EdgeInsets.only(top: 20, left: 12, right: 12),
-        child: Container(
-          alignment: Alignment.center,
-          height: 55.72,
-          decoration: BoxDecoration(
-            color: AppColor.lightGray,
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: TextField(
-            onChanged: onSearch,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              height: 1.2,
+    return Theme(
+      data: ThemeData(),
+      child: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.only(top: 20, left: 12, right: 12),
+          child: Container(
+            alignment: Alignment.center,
+            height: 55.72,
+            decoration: BoxDecoration(
+              color: AppColor.lightGray,
+              borderRadius: BorderRadius.circular(15),
             ),
-            decoration: InputDecoration(
-              prefixIcon: const Icon(
-                Icons.search,
-                color: Colors.white70,
-                size: 22,
-              ),
-              hintText: "Search",
-              hintStyle: context.fonts.titleMedium?.copyWith(
-                color: Colors.white54,
+            child: TextField(
+              cursorColor: AppColor.yellow,
+              onChanged: onSearch,
+              style: const TextStyle(
+                color: Colors.white,
                 fontSize: 16,
+                height: 1.2,
               ),
-              border: InputBorder.none,
-              contentPadding: const EdgeInsets.symmetric(vertical: 14),
+              decoration: InputDecoration(
+
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.white70,
+                  size: 22,
+                ),
+                hintText: "Search",
+                hintStyle: context.fonts.titleMedium?.copyWith(
+                  color: Colors.white54,
+                  fontSize: 16,
+                ),
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(vertical: 14),
+              ),
             ),
           ),
         ),
